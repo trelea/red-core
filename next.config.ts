a0872e83b497a222af96c7544aade32ed6395d99
trelea dev <treleamarius76@gmail.com>
2026-03-08 12:51:40 +0200

    TLS

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
